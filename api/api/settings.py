3911941875fc61f1fd033b81d46f5a2d81a5b129
jmugisha1 <144003057+jmugisha1@users.noreply.github.com>
2025-08-03 21:36:33 +0100

    Update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -127,4 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # Adjust this to your frontend URL
+    "https://mlops-ui.vercel.app/"
 ]
